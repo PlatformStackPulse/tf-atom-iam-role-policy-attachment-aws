349c78fd0400828e39d88321591c375d83045251
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 20:35:06 +0100

    feat: implement tf-atom-iam-role-policy-attachment-aws module

--- variables.tf
+++ variables.tf
@@ -1,7 +1,19 @@
-# -----------------------------------------------------------------------------
-# Module-Specific Variables
-#
-# Note: Standard labeling variables (enabled, namespace, tenant, environment,
-# stage, name, delimiter, attributes, tags, label_order, etc.) are provided
-# by context.tf via the tf-label module.
-# -----------------------------------------------------------------------------
+variable "role_name" {
+  description = "Name of the IAM role to attach the policy to"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9+=,.@_-]+$", var.role_name))
+    error_message = "role_name must contain only valid IAM role name characters."
+  }
+}
+
+variable "policy_arn" {
+  description = "ARN of the IAM policy to attach"
+  type        = string
+
+  validation {
+    condition     = can(regex("^arn:aws:iam::", var.policy_arn))
+    error_message = "policy_arn must be a valid IAM policy ARN."
+  }
+}
